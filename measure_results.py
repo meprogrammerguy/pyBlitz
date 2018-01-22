2a8aa1f3bdc2fd2ad78717f81fad1a7b65c4371e
meprogrammerguy <b65535@gmail.com>
2018-01-22 13:56:34 -0600

    debug session, need to add abbreviations to sched scraper

--- measure_results.py
+++ measure_results.py
@@ -11,6 +11,10 @@ from pathlib import Path
 from datetime import datetime
 import re
 
+def GetPercent(item):
+    newstr = item.replace("%", "")
+    return float(newstr)
+
 def GetIndex(item):
     idx = re.findall(r'\d+', str(item))
     if (len(idx) == 0):
@@ -69,51 +73,29 @@ for file in week_files:
         for row in reader:
             list_week.append(row)
 
+index = 0
 for idx in range(len(list_sched)):
-    index = 0
     total = 0
     skip = 0
     correct = 0
-    loop = 0
+    print ("week{0}".format(idx + 1))
     for item in list_sched[idx].values():
         total += 1
-        scorea, scoreb = GetActualScores(item["Score"])
-        chancea = GetIndex(list_week[loop]["ChanceA"])
-        chanceb = GetIndex(list_week[loop]["ChanceB"])
-        loop += 1
-        if (chancea < 0 or chanceb < 0 or scorea < 0 or scoreb < 0):
+        scoreb, scorea = GetActualScores(item["Score"])
+        #pdb.set_trace()
+        chancea = GetPercent(list_week[index]["ChanceA"])
+        index += 1
+        if (chancea < 0 or scorea < 0 or scoreb < 0):
             print ("skip " + item["Score"])
             skip += 1
         else:
-            print ("not skip " + item["Score"])
-            if (chancea > 50 and scorea > scoreb):
-                print ("1) chance={0}, scorea={1}, scoreb={2}".format(chancea, scorea, scoreb))
+            print ("not skip {0}% - {1}".format(chancea, item["Score"]))
+            if (chancea >= 50 and (scorea > scoreb)):
+                print ("1) chancea={0}, scorea={1}, scoreb={2}".format(chancea, scorea, scoreb))
                 correct += 1
-            if (chanceb > 50 and scoreb > scorea):
-                print ("2) chance={0}, scorea={1}, scoreb={2}".format(chanceb, scorea, scoreb))
+            if (chancea < 50 and (scoreb > scorea)):
+                print ("2) chancea={0}, scorea={1}, scoreb={2}".format(chancea, scorea, scoreb))
                 correct += 1
     print ("total={0}, skip={1}, correct={2}".format(total, skip, correct))
     pdb.set_trace()
-
-merge_sheet = open('merge_schedule.csv', 'w', newline='')
-csvwriter = csv.writer(merge_sheet)
-dict_merge = OrderedDict()
-dict_merge["match ratio"] = []
-dict_merge["stats team"] = []
-dict_merge["scheduled team"] = []
-dict_merge["corrected stats team"] = []
-values = []
-for item in sched_teams:
-    key = process.extractOne(item, stats_teams, scorer=fuzz.QRatio)
-    dict_merge["match ratio"].append(key[1])
-    dict_merge["stats team"].append(key[0])
-    dict_merge["scheduled team"].append(item)
-    dict_merge["corrected stats team"].append("")
-    values.append([key[1], key[0], item,  ""])
-
-
-csvwriter.writerow(dict_merge.keys())
-for value in values:
-    csvwriter.writerow(value)
-merge_sheet.close()
 print ("done.")
