9220597a2007abb635701ca9b277ced667bc059b
meprogrammerguy <b65535@gmail.com>
2019-10-18 08:29:01 -0500

    fixed LSU mix up

---
+++ data/settings.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+data_path = "data/"
+defaults_path = "defaults/"
+predict_root = "predict/"
+predict_sched = "schedule/"
+predict_saved = "saved/"
+homeAdvantage = 7.897
+exceptions = []
