fab6660c5f9fcebe6a3cbc256544babfa50d2ae0
meprogrammerguy <b65535@gmail.com>
2019-11-25 08:30:59 -0600

    recursive permissions

--- scrape_schedule.py
+++ scrape_schedule.py
@@ -50,8 +50,8 @@ def main(argv):
     print ("**************************")
 
     Path(path).mkdir(parents=True, exist_ok=True) 
-    os.chmod(settings.predict_root, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
-    os.chmod(path, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
+    #os.chmod(settings.predict_root, stat.S_ISGID | stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
+    #os.chmod(path, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
     for p in Path(path).glob("sched*.*"):
         p.unlink()
 
@@ -168,8 +168,13 @@ def main(argv):
                     count += 1
                 csvwriter.writerow(row.values())
             sched_sheet.close()
-    os.chmod(settings.predict_root, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
-    os.chmod(path, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
+    #os.chmod(settings.predict_root, stat.S_ISGID | stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
+    #os.chmod(path, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
+    for root, dirs, files in os.walk(settings.predict_root):
+        for d in dirs:
+            os.chmod(os.path.join(root, d), stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
+        for f in files:
+            os.chmod(os.path.join(root, f), stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
     print ("done.")
 
 if __name__ == "__main__":
