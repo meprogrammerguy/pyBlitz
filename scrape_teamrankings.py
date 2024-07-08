6c74bcf7dcca4e9b648448f7c1f87e247f2018a0
meprogrammerguy <b65535@gmail.com>
2024-07-08 05:28:58 -0500

    fix warnings, excel spreadsheet for teamrankings

--- scrape_teamrankings.py
+++ scrape_teamrankings.py
@@ -42,28 +42,28 @@ E=[]
 index=0
 for row in ratings_table[0].findAll("tr"):
     col=row.findAll('td')
-    if len(col)>0 and col[1].find(text=True)!="Team":
+    if len(col)>0 and col[1].find(string=True)!="Team":
         index+=1
         IDX.append(index)
-        A.append(pyBlitz.CleanString(col[1].find(text=True)))
-        B.append(col[3].find(text=True))
+        A.append(pyBlitz.CleanString(col[1].find(string=True)))
+        B.append(col[3].find(string=True))
 for team in A:
     for row in ratings_table[1].findAll("tr"):
         col=row.findAll('td')
-        if len(col)>0 and col[1].find(text=True)==team:
-            C.append(col[3].find(text=True))
+        if len(col)>0 and col[1].find(string=True)==team:
+            C.append(col[3].find(string=True))
             break
 for team in A:
     for row in ratings_table[2].findAll("tr"):
         col=row.findAll('td')
-        if len(col)>0 and col[1].find(text=True)==team:
-            D.append(col[3].find(text=True))
+        if len(col)>0 and col[1].find(string=True)==team:
+            D.append(col[3].find(string=True))
             break
 for team in A:
     for row in ratings_table[3].findAll("tr"):
         col=row.findAll('td')
-        if len(col)>0 and col[1].find(text=True)==team:
-            E.append(col[3].find(text=True))
+        if len(col)>0 and col[1].find(string=True)==team:
+            E.append(col[3].find(string=True))
             break
 
 df=pd.DataFrame(IDX,columns=['Index'])
@@ -77,16 +77,8 @@ Path(settings.data_path).mkdir(parents=True, exist_ok=True)
 with open(settings.data_path + 'teamrankings.json', 'w') as f:
     f.write(df.to_json(orient='index'))
 
-with open(settings.data_path + "teamrankings.json") as stats_json:
-    dict_stats = json.load(stats_json, object_pairs_hook=OrderedDict)
-stats_sheet = open(settings.data_path + 'teamrankings.csv', 'w', newline='')
-csvwriter = csv.writer(stats_sheet)
-count = 0
-for row in dict_stats.values():
-    if (count == 0):
-        header = row.keys()
-        csvwriter.writerow(header)
-        count += 1
-    csvwriter.writerow(row.values())
-stats_sheet.close()
+writer = pd.ExcelWriter(settings.data_path + "teamrankings.xlsx", engine="xlsxwriter")
+df.to_excel(writer, sheet_name="Sheet1", index=False)
+writer.close()
+
 print ("done.")
